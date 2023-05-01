3e80906da24c25f7f67d114172b4b1ca109936af
juandsoto <juandsoto2001@hotmail.com>
2023-04-30 22:00:04 -0500

    fix: ver materias por estudiante

--- app/src/components/EstudianteList.jsx
+++ app/src/components/EstudianteList.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 
 export const EstudianteList = ({ estudiantes, onEdit, onDelete }) => {
 	const [matriculas, setMatriculas] = useState([]);
@@ -8,8 +9,8 @@ export const EstudianteList = ({ estudiantes, onEdit, onDelete }) => {
 	const handleViewMatriculas = async (estudiante) => {
 		setSelectedEstudiante(estudiante);
 		try {
-			const response = await axios.get(`/api/matriculas?estudiante_id=${estudiante.id}`);
-			setMatriculas(response.data);
+			const response = await axios.get(`http://localhost:8000/ver_materias_en_estudiante/${estudiante.id}/`);
+			setMatriculas(response.data.materias_matriculadas);
 			setShowMatriculas(true);
 		} catch (error) {
 			console.error(error);
@@ -23,55 +24,55 @@ export const EstudianteList = ({ estudiantes, onEdit, onDelete }) => {
 	return (
 		<div>
 			<h2>Lista de estudiantes</h2>
-			<table>
-				<thead>
-					<tr>
-						<th>ID</th>
-						<th>Nombre</th>
-						<th>Edad</th>
-						<th>Correo Electrónico</th>
-						<th>Acciones</th>
-					</tr>
-				</thead>
-				<tbody>
-					{ estudiantes.map((estudiante) => (
-						<tr key={ estudiante.correo_electronico }>
-							<td>{ estudiante.id }</td>
-							<td>{ estudiante.nombre }</td>
-							<td>{ estudiante.edad }</td>
-							<td>{ estudiante.correo_electronico }</td>
-							<td>
-								<button onClick={ () => onEdit(estudiante) }>Editar</button>
-								<button onClick={ () => onDelete(estudiante) }>Eliminar</button>
-							</td>
+			<div style={ { display: "flex", gap: 4 } }>
+				<table>
+					<thead>
+						<tr>
+							<th>ID</th>
+							<th>Nombre</th>
+							<th>Edad</th>
+							<th>Correo Electrónico</th>
+							<th>Acciones</th>
 						</tr>
-					)) }
-				</tbody>
-			</table>
-			{ showMatriculas && (
-				<div>
-					<h2>Matrículas de { selectedEstudiante.nombre }</h2>
-					<table>
-						<thead>
-							<tr>
-								<th>ID</th>
-								<th>Estudiante</th>
-								<th>Materia</th>
+					</thead>
+					<tbody>
+						{ estudiantes.map((estudiante) => (
+							<tr key={ estudiante.correo_electronico }>
+								<td>{ estudiante.id }</td>
+								<td>{ estudiante.nombre }</td>
+								<td>{ estudiante.edad }</td>
+								<td>{ estudiante.correo_electronico }</td>
+								<td>
+
+									<button onClick={ () => handleViewMatriculas(estudiante) }>Ver matrículas</button>
+									<button onClick={ () => onEdit(estudiante) }>Editar</button>
+									<button onClick={ () => onDelete(estudiante) }>Eliminar</button>
+								</td>
 							</tr>
-						</thead>
-						<tbody>
-							{ matriculas.map((matricula) => (
-								<tr key={ matricula.id }>
-									<td>{ matricula.id }</td>
-									<td>{ matricula.estudiante }</td>
-									<td>{ matricula.materia }</td>
+						)) }
+					</tbody>
+				</table>
+				{ showMatriculas && (
+					<div>
+						<h2>Matrículas de { selectedEstudiante.nombre }</h2>
+						<table>
+							<thead>
+								<tr>
+									<th>Materia</th>
 								</tr>
-							)) }
-						</tbody>
-					</table>
-					<button onClick={ handleHideMatriculas }>Cerrar</button>
-				</div>
-			) }
+							</thead>
+							<tbody>
+								{ matriculas.map((matricula) => (
+									<tr key={ matricula }>
+										<td>{ matricula }</td>
+									</tr>
+								)) }
+							</tbody>
+						</table>
+						<button onClick={ handleHideMatriculas }>Cerrar</button>
+					</div>
+				) }
+			</div>
 		</div>
 	);
 };
