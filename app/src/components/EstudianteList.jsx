0fac76a163af9ab6f81519393bb26c111f89c921
juandsoto <juandsoto2001@hotmail.com>
2023-04-30 21:46:04 -0500

    chatgpt: ver materias por estudiante

--- app/src/components/EstudianteList.jsx
+++ app/src/components/EstudianteList.jsx
@@ -1,6 +1,25 @@
 import React from "react";
 
 export const EstudianteList = ({ estudiantes, onEdit, onDelete }) => {
+	const [matriculas, setMatriculas] = useState([]);
+	const [showMatriculas, setShowMatriculas] = useState(false);
+	const [selectedEstudiante, setSelectedEstudiante] = useState(null);
+
+	const handleViewMatriculas = async (estudiante) => {
+		setSelectedEstudiante(estudiante);
+		try {
+			const response = await axios.get(`/api/matriculas?estudiante_id=${estudiante.id}`);
+			setMatriculas(response.data);
+			setShowMatriculas(true);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	const handleHideMatriculas = () => {
+		setShowMatriculas(false);
+	};
+
 	return (
 		<div>
 			<h2>Lista de estudiantes</h2>
@@ -29,6 +48,30 @@ export const EstudianteList = ({ estudiantes, onEdit, onDelete }) => {
 					)) }
 				</tbody>
 			</table>
+			{ showMatriculas && (
+				<div>
+					<h2>Matrículas de { selectedEstudiante.nombre }</h2>
+					<table>
+						<thead>
+							<tr>
+								<th>ID</th>
+								<th>Estudiante</th>
+								<th>Materia</th>
+							</tr>
+						</thead>
+						<tbody>
+							{ matriculas.map((matricula) => (
+								<tr key={ matricula.id }>
+									<td>{ matricula.id }</td>
+									<td>{ matricula.estudiante }</td>
+									<td>{ matricula.materia }</td>
+								</tr>
+							)) }
+						</tbody>
+					</table>
+					<button onClick={ handleHideMatriculas }>Cerrar</button>
+				</div>
+			) }
 		</div>
 	);
 };
